6b88b304ef6cb56ff4057114af2e746e2b2c0b1d
GertTijink <920809544@has.nl>
2017-11-20 10:10:31 +0100

    OpenLayers set up

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Engineer 2 OpenLayers</title>
+		<link href="ol/os.css" rel="stylesheet"/>
+		<link href="css/engineer2.css" rel="stylesheet"/>
+	</head>
+	<body>
+		<div id="kaart"</div>
+		<h1>Welkom op mijn OpenLayers pagina</h1>
+	</body>
+</html>	
